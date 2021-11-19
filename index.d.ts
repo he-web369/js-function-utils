2577d13a45f4feef078cc2ebd818aebc1da4129a
unknown <shulun.he@yoonop.net>
2021-11-19 16:46:48 +0800

    modify deepClone arg type

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,8 @@
  * @Author: hsl
  * @Descripttion: 
  * @Date: 2021-11-19 16:23:44
- * @LastEditTime: 2021-11-19 16:26:05
+ * @LastEditTime: 2021-11-19 16:45:49
  */
 export interface utils {
-    deepClone(obj: Object): object;
+    deepClone(obj: object): object;
 }
